98df446ac2b5aa47cce8fd6ae14345984cd0a3bb
John Jackson <jcj52@nau.edu>
2020-08-25 14:02:16 -0700

    added file extension check

--- ppm_read_write.c
+++ ppm_read_write.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void fail(char *s)
 {
-    printf("Error: %s\n\n", s);
+    printf("\nError: %s\n\n", s);
     printf("Usage:\n");
     printf("./ppmrw FORMAT_# INPUT_PPM OUTPUT_PPM\n\n");
     exit(1);
@@ -11,15 +12,23 @@ void fail(char *s)
 
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    if (argc != 4) // check correct number of arguments
     {
         fail("Wrong number of arguments.");
     }
 
     int format = atoi(argv[1]);
 
-    if ((format != 3) && (format != 6))
+    if ((format != 3) && (format != 6)) // check magic number = 3 or 6
     {
         fail("PPM format must be either 3 or 6.");
     }
+
+    if ((strstr(argv[2], ".ppm") == NULL) || (strstr(argv[3], ".ppm") == NULL)) // check file extensions
+    {
+        fail("Input and output files must be of type .ppm.");
+    }
+
+    FILE *infile = fopen(argv[2], "rb");
+    FILE *outfile = fopen(argv[3], "wb");
 }
