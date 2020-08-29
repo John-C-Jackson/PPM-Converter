2d877ce5f52974712c3ba648e7c6c8d4925b8b85
John Jackson <jcj52@nau.edu>
2020-08-28 18:44:13 -0700

    deleting variables in main, adding structs

--- ppm_read_write.c
+++ ppm_read_write.c
@@ -4,6 +4,20 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+typedef struct Pixel
+{
+    unsigned char r;
+    unsigned char g;
+    unsigned char b;
+} pixel;
+
+typedef struct PPM
+{
+    int width;
+    int height;
+    struct Pixel pixel;
+} ppm;
+
 void fail(char *s)
 {
     printf("\nError: %s\n\n", s);
@@ -14,8 +28,15 @@ void fail(char *s)
 
 unsigned int *read_p3(FILE *file_in, int width, int height)
 {
-    // I think everything in read_p3 and read_p6 will be inside
-    // a while loop -> while not at end of file
+    int i, j, pixmap_size;
+    uint8_t *pixmap, *pixel;
+
+    // allocate memory for pixmap
+    pixmap_size = width * height * 3;
+    pixmap = malloc(pixmap_size);
+    pixel = pixmap;
+
+    // read P3
     return 0;
 }
 
@@ -24,24 +45,30 @@ unsigned int *read_p6(FILE *file_in, int width, int height)
     return 0;
 }
 
-int write_p3(FILE *file_out) // this and write_p6 might have an additional argument (int *pixmap)
+void write_p3(unsigned char *file_out) // this and write_p6 might have an additional argument (int *pixmap)
 {
     // fprintf
-    return 0;
+    return;
 }
 
-int write_p6(FILE *file_out)
+void write_p6(unsigned char *file_out)
 {
-    return 0;
+    /*
+    fwrite(pixmap, 1, bar_width * 7 * bar_height * 4, file);
+    fclose(file);
+    */
+    return;
 }
 
 int main(int argc, char *argv[])
 {
     const int MAX = 100;
-    char buffer[100], hash, magic;
-    char *magic_number, *line, *color;
-    int maxcolor, dimensions, width, height;
+    int maxcolor, dimensions, width, height, out_format;
+    char buffer[100], hash;
+    char *line;
     bool p3 = false, p6 = false;
+    FILE *infile;
+    const char *outfile;
 
     // check correct number of arguments
     if (argc != 4)
@@ -49,10 +76,10 @@ int main(int argc, char *argv[])
         fail("Wrong number of arguments.");
     }
 
-    int format = atoi(argv[1]);
+    out_format = atoi(argv[1]);
 
     // check user-supplied format is 3 or 6
-    if ((format != 3) && (format != 6))
+    if ((out_format != 3) && (out_format != 6))
     {
         fail("PPM format must be either 3 or 6.");
     }
@@ -63,20 +90,25 @@ int main(int argc, char *argv[])
         fail("Input and output files must be of type .ppm.");
     }
 
-    FILE *infile = fopen(argv[2], "rb");
-    FILE *outfile = fopen(argv[3], "wb");
+    infile = fopen(argv[2], "rb");
+    outfile = argv[3];
+
+    if (infile == NULL)
+    {
+        fail("The input file is empty.");
+    }
 
     // get magic number
-    magic_number = fgets(buffer, MAX, infile);
-    if (strcmp(magic_number, "P3\n") == 0)
+    line = fgets(buffer, MAX, infile);
+    if (strcmp(line, "P3\n") == 0)
     {
         p3 = true;
     }
-    else if (strcmp(magic_number, "P6\n") == 0)
+    else if (strcmp(line, "P6\n") == 0)
     {
         p6 = true;
     }
-    printf("%s\n", magic_number);
+    printf("%s\n", line);
 
     // pass over comments
     while (1)
@@ -96,10 +128,10 @@ int main(int argc, char *argv[])
     printf("%u\n", height);
 
     // get max color value
-    color = fgets(buffer, MAX, infile);
-    printf("%s\n", color);
+    line = fgets(buffer, MAX, infile);
+    printf("%s\n", line);
 
-    if (strncmp(color, "255", 3) != 0)
+    if (strncmp(line, "255", 3) != 0)
     {
         fail("Wrong max color.\n");
     }
@@ -107,13 +139,13 @@ int main(int argc, char *argv[])
     // call either read_p6 or read_p3
     if (p3)
     {
-        printf("convert data to P%d\n", format);
+        printf("convert data to P%d\n", out_format);
         printf("call read_p3\n");
         read_p3(infile, width, height);
     }
     else if (p6)
     {
-        printf("convert data to P%d\n", format);
+        printf("convert data to P%d\n", out_format);
         printf("call read_p6\n");
         read_p6(infile, width, height);
     }
